e267a796a7eba554ae76ba8a6970f40898c0aba7
mihailalekseev <MkiS1KbImz>
2021-02-14 17:23:49 +0300

    Added recognition algorithm

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import cv2
+import os
+from imutils import contours
+import pytesseract
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# process image
+file_names = os.listdir("images/todo/")
+file_names.sort()
+for file in file_names:
+    print(file)
+    image = cv2.imread("images/todo/" + file)
+    height, width, _ = image.shape
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("images/done/" + file + "_grey.jpg", gray_image)
+    image_without_thrash = cv2.threshold(gray_image, 100, 255, cv2.THRESH_BINARY)[1]
+    cv2.imwrite("images/done/" + file + "_grey_otsu_100.jpg", image_without_thrash)
+    cnts = cv2.findContours(image_without_thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts, _ = contours.sort_contours(cnts[0])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for c in cnts:
+        area = cv2.contourArea(c)
+        x, y, w, h = cv2.boundingRect(c)
+        # поиск номера по длине контура, значение 5000 нужно реглировать и искать оптимальное
+        if area > 5000:
+            img = image_without_thrash[y:y+h, x:x+w]
+            cv2.imwrite("images/done/" + file + "_licence_plate.jpg", img)
+            result = pytesseract.image_to_string(img, lang="rus+eng")
+            if len(result) > 7:
+                print(area)
+
+
+    # Нужно эксперементировать и подбирать кодировку под определенные уловия маста получения изображения
+    # Также стоит прогонять изображение несколько раз обработанное разными фильтрами
+    # image_without_thrash = cv2.threshold(gray_image, 80, 255, cv2.THRESH_BINARY)[1]
+    # cv2.imwrite("images/done/" + file + "_grey_otsu_80.jpg", image_without_thrash)
+    #
+    # image_without_thrash = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)[1]
+    # cv2.imwrite("images/done/" + file + "_grey_otsu_127.jpg", image_without_thrash)
+    #
+    # image_without_thrash = cv2.threshold(gray_image, 140, 255, cv2.THRESH_BINARY)[1]
+    # cv2.imwrite("images/done/" + file + "_grey_otsu_140.jpg" + file, image_without_thrash)
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+pip install imutils
+pip install opencv-python
+pip install pytesseract
+
+sudo apt-get update
+sudo apt-get install libleptonica-dev
+sudo apt-get install tesseract-ocr tesseract-ocr-dev
+sudo apt-get install libtesseract-dev
